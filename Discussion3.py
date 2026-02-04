59cab23a18f30baecca26055519843c7156fa39a
lynn van <lynnvan@lynns-MacBook-Air.local>
2026-02-03 21:47:25 -0500

    Completed __init__ method

--- Discussion3.py
+++ Discussion3.py
@@ -2,30 +2,32 @@ import math
 
 class Rectangle():
     # Create the constructor "__init__" method
-
+    def __init__(self, width, height):
     # YOUR CODE HERE
-
+        self.width = width
+        self.height = height
 
 
     # Create the "__str__" method
-
+    def __str__ (self):
     # YOUR CODE HERE
+        return f"Rectangle with width {self.width} and height {self.height}"
 
 
 
     # Create the "area_calculator" method
-
-    # YOUR CODE HERE
+    def area_calculator(self):
+            # YOUR CODE HERE
+        return self.width * self.height
 
 
 
     # Create the "__eq__" method
-    # 
-    # Returns a boolean value
-
-    # YOUR CODE HERE
-
-
+    def __eq__(self, other):
+        # Returns a boolean value
+        if isinstance(other, Rectangle):
+            return self.width == other.width and self.height == other.height
+        return False
     
 
 
@@ -36,14 +38,12 @@ def main():
     # call the area_calculator method
     print("Area:", r1.area_calculator())
 
-
     r2 = Rectangle(10, 15)
     print(r2)
     print("Area:", r2.area_calculator())
-    # call the __eq__ method
+        # call the __eq__ method
     print(r1 == r2)
     print()
-
     # you can create additional rectangle objects to 
     # test your code or learn more about how the class behaves
     pass
